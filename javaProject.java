67f248e618f89bf66f78a73da3f5c644d25047e3
Mitch Colyer <Mitch@Mitchs-MacBook-Air.local>
2015-11-04 22:13:57 -0700

    Please Work

--- javaProject.java
+++ javaProject.java
@@ -1,11 +1,53 @@
-/**
- * Created by Mitch on 11/4/15.
- */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class javaProject {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        javaProject obj = new javaProject();
+        obj.run();
+
+    }
+
+    public void run() {
+
+        String csvFile = "/ctct_revenue_oct2015.csv";
+        BufferedReader br = null;
+        String line = "";
+        String csvSplitBy = "|";
+
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                String[] value = line.split(csvSplitBy);
+
+                System.out.println(value);
+
+            }
+
+
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (br != null) {
+                try {
+                    br.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
